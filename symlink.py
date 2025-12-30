d4ee3b8e311d4d54b61cdd3858cb5999f4ed252e
wacky <wacky612@gmail.com>
2025-12-30 12:54:46 +0900

    サムネイル動画の最初と最後にblackを入れるように、symlink.pyを修正

--- symlink.py
+++ symlink.py
@@ -4,9 +4,16 @@ import os
 import json
 from pathlib import Path
 
-data = json.loads(Path('json/data.json').read_text())
+data  = json.loads(Path('json/data.json').read_text())
+blank = f'{os.getcwd()}/thumbnail/black.png'
 
-i = 1
+i = 0
+
+dst = f'{os.getcwd()}/thumbnail/link/{i:05}.png'
+if os.path.lexists(dst): os.remove(dst)
+os.symlink(blank, dst)
+
+i = i + 1
 
 for c in range(0, len(data)):
     for w in range(0, len(data[c]['Worlds'])):
@@ -17,7 +24,9 @@ for c in range(0, len(data)):
             src = f'{os.getcwd()}/thumbnail/img/{data[c]['Worlds'][w]['ID']}.png'
             os.symlink(src, dst)
         else:
-            src = f'{os.getcwd()}/thumbnail/black.png'
-            os.symlink(src, dst)
-        
+            os.symlink(blank, dst)
         i = i + 1
+
+dst = f'{os.getcwd()}/thumbnail/link/{i:05}.png'
+if os.path.lexists(dst): os.remove(dst)
+os.symlink(blank, dst)
